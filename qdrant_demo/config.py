152714208cdb51105f9ea91c452708a6291a4043
John Just <johnpjust@gmail.com>
2024-07-03 23:56:06 -0500

    updates

--- qdrant_demo/config.py
+++ qdrant_demo/config.py
@@ -5,7 +5,8 @@ ROOT_DIR = os.path.dirname(CODE_DIR)
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 STATIC_DIR = os.path.join(ROOT_DIR, "static")
 
-QDRANT_URL = os.environ.get("QDRANT_URL", "http://localhost:6333/")
+# QDRANT_URL = os.environ.get("QDRANT_URL", "http://localhost:6333/") #http
+QDRANT_URL = os.environ.get("QDRANT_URL", "http://localhost:6334/") #grpc
 QDRANT_API_KEY = os.environ.get("QDRANT_API_KEY", "")
 
 COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "text-demo")
