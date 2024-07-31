ef9c6fec503d59b4429d8aafb6eba61ff14436b0
John Just <JustJohnP@JohnDeere.com>
2024-07-31 18:17:43 -0500

    update

--- frontend/src/components/InteractiveTable/index.tsx
+++ frontend/src/components/InteractiveTable/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import { FC, useState } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
